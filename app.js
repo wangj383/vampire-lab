2f8153a9429d202d84a0442fa85ea6d1c9eb1a8e
Yvon Wang <yvonwang@Yvons-MacBook.local>
2020-03-29 20:30:35 -0400

    finished

--- app.js
+++ app.js
@@ -130,105 +130,171 @@ setTimeout(() => {
 
 // Q1
 Vampire.find({gender: 'f'})
-// .then (v => {
-//   console.log(v)
-// })
-// .catch(err => {
-//   console.error(err)
-// })
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q2
 
 Vampire.find({victims: { $gt: 500}})
-// .then(v => {
-//     console.log(v)
-// }).catch(err => {
-//     console.error(err)
-// })
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // alternative function:
 // Vampire.find().where('victims').gt(500)
 
 
 // Q3
 Vampire.find({victims: { $lte: 150}})
-// .then(v => {
-//     console.log(v)
-// }).catch(err => {
-//     console.error(err)
-// })
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 
 // Q4
 Vampire.find({victims: { $ne: 210234}})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q5
 Vampire.find({ $and: [{victims:{$gt: 150}},{victims: {$lt: 500}}]})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
 
 
 // Q1
 Vampire.find({title: { $exists: true}})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q2
 Vampire.find({victims: { $exists: false}})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q3
 Vampire.find({ $and: [{victims:{$exists: false}},{title: {$exists: true}}]})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q4
 Vampire.find({ $and: [{victims:{$exists: false}},{victims: {$gt: 1000}}]})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 
 /////////////////////////////////////////////////
 // ### Select with OR
 // Q1
 Vampire.find({ location: {$in: ['New York, New York, US', 'New Orleans, Louisiana, US']}})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q2
 Vampire.find({ loves: {$in: ['brooding', 'being tragic']}})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 
 // Q3
 Vampire.find({ $or: [{victims: {$gt: 1000}}, { loves: 'marshmallows'}] })
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q4
 Vampire.find({ $or: [{hair_color: 'red'}, {eye_color: "green"}] })
-// .then(v => {
-//   console.log(v)
-// }).catch(err => {
-//   console.error(err)
-// })
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 /////////////////////////////////////////////////
 //### Select objects that match one of several values
 // Q1
 Vampire.find({ loves: {$in: ['frilly shirtsleeves','frilly collars']} })
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 
 // Q2
 Vampire.find({ loves: 'brooding'})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q3
 Vampire.find({ loves: {$in: [ "appearing innocent", "trickery", "lurking in rotting mansions" , "R&B music"]} })
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q4
 Vampire.find({ $and:[ {loves: "fancy cloaks"},{loves: {$nin: [ 'top hats', "virgin blood"]} } ]})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 /////////////////////////////////////////////////
 //### Negative Selection
 
 // Q1
 Vampire.find({ $and:[ {loves: "ribbons"},{eye_color: {$nin: ["brown"]}}]})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q2
 Vampire.find({ location: {$nin: ['Rome']}})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q3
 Vampire.find({ loves: {$nin: ["fancy cloaks", "frilly shirtsleeves", "appearing innocent", "being tragic", "brooding"]}})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 // Q4
 Vampire.find({ victims: {$lte: 200 }})
-
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## REPLACE
@@ -236,7 +302,8 @@ Vampire.findOneAndUpdate(
   {name: "Claudia"}, 
   {$set: {name:'Eve' }},
   {new: true},
-).then(v => {
+)
+.then(v => {
   console.log(v)
 }).catch(err => {
   console.error(err)
@@ -245,10 +312,62 @@ Vampire.findOneAndUpdate(
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## UPDATE
+// Q1
+Vampire.findOneAndUpdate(
+  {name: "Eve"}, 
+  {$set: {gender:'m' }},
+  {new: true},
+)
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 
+// Q2
+Vampire.findOneAndUpdate(
+  {name: "Eve"}, 
+  {$set: {name:'Moniker' }},
+  {new: true},
+)
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
+
+// Q3
+Vampire.find( { gender: 'f' } ).update( { $set: { gender: 'fems' } } )
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## REMOVE
+// Q1
+Vampire.findOneAndRemove({hair_color: 'brwon'})
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
+
+// Q2
+Vampire.find({eye_color: 'blue'}).remove()
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
+
+Vampire.find({eye_color: 'blue'})
+.then(v => {
+  console.log(v)
+}).catch(err => {
+  console.error(err)
+})
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
